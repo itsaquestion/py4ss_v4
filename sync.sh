0154d058d2f3075e80bc869507dad5fda946b378
itsaquestion <itsaquestion@gmail.com>
2024-05-27 02:04:38 +0800

    服务器搬迁

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
-rsync -av -e ssh _book/ lee@cc.imtass.me:/www/py4ss.net/python 
+rsync -av -e ssh _book/ lee@hk.imtass.me:/www/py4ss.net/python 
 
 # # turn off cache = development_mode on 
 # curl -X PATCH "https://api.cloudflare.com/client/v4/zones/7e61015d60a07937b261e3314862c7a4/settings/development_mode" \
